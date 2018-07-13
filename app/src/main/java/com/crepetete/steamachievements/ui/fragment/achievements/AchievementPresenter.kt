6cd94a81ebf8cb5dfabf7a2b27110709f4885cad
Patrick van de Graaf <p.vandegraaf@yipyip.nl>
2018-07-13 09:52:31 +0200

    - Achievement values now persist loading and unloading view - Now using String.format and DecimalFormat to show a completion percentage up to 3 digits after the comma

--- app/src/main/java/com/crepetete/steamachievements/ui/fragment/achievements/AchievementPresenter.kt
+++ app/src/main/java/com/crepetete/steamachievements/ui/fragment/achievements/AchievementPresenter.kt
@@ -7,6 +7,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
+import java.util.*
 import javax.inject.Inject
 
 class AchievementPresenter(achievementsView: AchievementsView)
@@ -26,34 +27,40 @@ class AchievementPresenter(achievementsView: AchievementsView)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     view.setTotalAchievementsInfo(it.filter { it.achieved }.size)
-                    val achievementsForGame = HashMap<String, MutableList<Achievement>>()
-                    it.forEach { achievement ->
-                        if (achievementsForGame.containsKey(achievement.appId)) {
-                            achievementsForGame[achievement.appId]?.add(achievement)
-                        } else {
-                            achievementsForGame[achievement.appId] = mutableListOf()
-                        }
-                    }
-
-                    val percentages = mutableListOf<Double>()
-                    achievementsForGame.keys.forEach { key ->
-                        val allAchievements = achievementsForGame[key]?.toList()
-                        if (allAchievements != null) {
-                            val achievedAchievements = allAchievements.filter {
-                                it.achieved
-                            }
-                            if (achievedAchievements.isNotEmpty()) {
-                                percentages.add((achievedAchievements.size.toDouble() / allAchievements.size.toDouble()) * 100.0)
-                            }
-                        }
-                    }
-                    var percentagesSum = 0.0
-                    percentages.forEach { percentagesSum += it }
-                    val percentage = (percentagesSum / (percentages.size * 100.0) * 100.0)
-                    view.setCompletionPercentage(percentage)
-
+                    calculateCompletionPercentage(it)
                 }, {
                     Timber.e(it)
                 }))
+    }
+
+    private fun calculateCompletionPercentage(achievements: List<Achievement>) {
+        val achievementsForGame = HashMap<String, MutableList<Achievement>>()
+        achievements.map {
+            if (!achievementsForGame.containsKey(it.appId)) {
+                achievementsForGame[it.appId] = mutableListOf(it)
+            } else {
+                achievementsForGame[it.appId]!!.add(it)
+            }
+        }
+
+        val percentages = mutableListOf<Double>()
+        achievementsForGame.keys.forEach { key ->
+            val allAchievements = achievementsForGame[key]?.toList()
+            if (allAchievements != null) {
+                val achievedAchievements = allAchievements.filter {
+                    it.achieved
+                }
+                if (achievedAchievements.isNotEmpty()) {
+                    percentages.add((achievedAchievements.size.toDouble()
+                            / allAchievements.size.toDouble()) * 100.0)
+                }
+            }
+        }
+
+
+        var percentagesSum = 0.0
+        percentages.forEach { percentagesSum += it }
+        val percentage = (percentagesSum / (percentages.size * 100.0) * 100.0)
+        view.setCompletionPercentage(percentage)
     }
 }
--- app/src/main/java/com/crepetete/steamachievements/ui/fragment/achievements/AchievementsFragment.kt
+++ app/src/main/java/com/crepetete/steamachievements/ui/fragment/achievements/AchievementsFragment.kt
@@ -10,11 +10,14 @@ import android.widget.TextView
 import com.crepetete.steamachievements.R
 import com.crepetete.steamachievements.base.BaseFragment
 import com.crepetete.steamachievements.ui.activity.helper.LoadingIndicator
+import java.text.DecimalFormat
 
 class AchievementsFragment : BaseFragment<AchievementPresenter>(), AchievementsView {
     companion object {
         const val TAG = "ACHIEVEMENTS_FRAGMENT"
         private const val KEY_PLAYER_ID = "KEY_PLAYER_ID"
+        private const val KEY_PERCENTAGE = "KEY_PERCENTAGE"
+        private const val KEY_ACHIEVEMENT_COUNT = "KEY_ACHIEVEMENT_COUNT"
 
         fun getInstance(playerId: String, loadingIndicator: LoadingIndicator): Fragment {
             return AchievementsFragment().apply {
@@ -29,6 +32,9 @@ class AchievementsFragment : BaseFragment<AchievementPresenter>(), AchievementsV
     private lateinit var textViewAllAchievements: TextView
     private lateinit var textViewCompletion: TextView
 
+    private var achievementCount = 0
+    private var completionPercentage = 0.0
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -37,15 +43,23 @@ class AchievementsFragment : BaseFragment<AchievementPresenter>(), AchievementsV
         textViewAllAchievements = view.findViewById(R.id.textview_total_achievements)
         textViewCompletion = view.findViewById(R.id.textview_completion)
 
+        if (achievementCount > 0){
+            setTotalAchievementsInfo(achievementCount)
+            setCompletionPercentage(completionPercentage)
+        }
+
         return view
     }
 
     override fun setTotalAchievementsInfo(achievementCount: Int) {
         textViewAllAchievements.text = "$achievementCount"
+        this.achievementCount = achievementCount
     }
 
     override fun setCompletionPercentage(percentage: Double) {
-        textViewCompletion.text = "$percentage%"
+        textViewCompletion.text = String.format(getString(R.string.percentage),
+                DecimalFormat("0.###").format(percentage))
+        completionPercentage = percentage
     }
 
     override fun instantiatePresenter(): AchievementPresenter {
